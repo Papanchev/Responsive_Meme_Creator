95adeecb57d2ea9f60e6103cfac3325a53095475
V.Papanchev <uwmbv@student.kit.edu>
2019-02-15 16:56:26 +0100

    Commiting project

---
+++ game1/game1.js
@@ -0,0 +1,85 @@
+
+
+var images = [
+    "/images/img0.jpg",
+    "/images/img1.jpg",
+    "/images/img2.jpg",
+    "/images/img3.jpg",
+    "/images/img4.jpg",
+    "/images/img5.jpg",
+    "/images/img6.jpg",
+    "/images/img7.jpg",
+]
+
+var curImgSrc = "";
+var curImg;
+var pictureLoaded = false;
+
+function pageLoad() {
+    /*
+    if (pictureLoaded) {
+        var myDiv = document.getElementById('div_img');
+        var img = document.createElement('img');
+        img.setAttribute("id", "img_id");
+        img.src = curImgSrc;
+        myDiv.appendChild(img);
+    }
+    */
+}
+
+
+
+document.getElementById('inputButton').onclick = function() {
+    /*
+    if (pictureLoaded) {
+        var myDiv = document.getElementById('div_img');
+        var img = document.createElement('img');
+        img.setAttribute("id", "img_id");
+        img.src = curImgSrc;
+        myDiv.appendChild(img);
+    }
+    */
+    
+    var upperText = document.forms["myForm"]["upText"].value;
+    var lowerText = document.forms["myForm"]["lowText"].value;
+
+    var img = curImg;
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    canvas.crossOrigin = "Anonymous";
+    canvas.width = img.width;
+    
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+
+    var font = (img.height + img.width ) * 0.05;
+    font = Math.ceil(font);
+
+    ctx.font = font + "pt Verdana";
+    ctx.fillStyle = "red";
+    var dy = img.height * 0.1;
+
+    ctx.fillText(upperText,40,dy + font);
+    ctx.fillText(lowerText,40,img.height - dy);
+   // console.log(ctx.getImageData(50, 50, 100, 100));
+}
+
+document.getElementById('btn').onclick = function() {
+    var myDiv = document.getElementById('div_img');
+    while(myDiv.firstChild) {
+        myDiv.removeChild(myDiv.firstChild);
+    }
+
+    var index = Math.floor(Math.random() * images.length); 
+    var img = document.createElement('img');
+    img.setAttribute("id", "img_id");
+    var loc = window.location.pathname;
+    var dir = loc.substring(0, loc.lastIndexOf('/'));
+    img.src = dir + images[index];
+
+    myDiv.appendChild(img);
+    curImg = img;
+    pictureLoaded = true;
+    document.getElementById('form_id').style.visibility = "visible";
+}
+
---
+++ game2/game2.js
@@ -0,0 +1,173 @@
+var strings = [
+    "Women..",
+    "Men..",
+    "I have an exam tomorrow",
+    "Man\'s best friend",
+    "I love trains",
+    "Mobile Computing",
+    "Flutter: It is just..",
+    "I can\'t think of anything..",
+]
+
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+
+var xsArr = new Array();
+var ysArr = new Array();
+var dragArr = new Array();
+var isDrawing;
+
+var redColor = "#FF0000";
+var blueColor = "#0000FF";
+var greenColor = "#008000";
+var yellowColor = "#FFD700";
+var purpleColor = "#800080"; // 	#663399
+
+var currentColor = redColor;
+var colorsArr = new Array();
+
+var smallSize = 2.5;
+var mediumSize = 5;
+var largeSize = 8;
+var hugeSize = 11;
+
+var currentSize = mediumSize;
+var sizesArr = new Array();
+
+var currentTool = "marker";
+
+document.getElementById('btn').onclick = function () {
+
+    var textDiv = document.getElementById('text_div');
+    while (textDiv.firstChild) {
+        textDiv.removeChild(textDiv.firstChild);
+    }
+
+    var index = Math.floor(Math.random() * strings.length);
+
+    var node = document.createTextNode("Your text is: ");
+    //   var textDiv = document.getElementById('text_div');
+    textDiv.appendChild(node);
+
+    var node2 = document.createTextNode(strings[index]);
+    textDiv.appendChild(node2);
+
+    // draw text on canvas
+
+    document.getElementById("save_btn").style.visibility = "visible";
+}
+
+canvas.onmousedown = function (e) {
+    var mouseX = e.pageX - this.offsetLeft;
+    var mouseY = e.pageY - this.offsetTop;
+
+    isDrawing = true;
+    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+    redraw();
+}
+
+canvas.onmousemove = function (e) {
+    if (isDrawing) {
+        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+        redraw();
+    }
+}
+
+canvas.onmouseup = function (e) {
+    isDrawing = false;
+}
+
+canvas.onmouseleave = function (e) {
+    isDrawing = false;
+}
+
+
+
+function addClick(x, y, dragging) {
+    xsArr.push(x);
+    ysArr.push(y);
+    dragArr.push(dragging);
+    if (currentTool == "eraser") {
+        colorsArr.push("white");
+    } else {
+        colorsArr.push(currentColor);
+    }
+    sizesArr.push(currentSize);
+}
+
+
+function redraw() {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+
+    //   context.strokeStyle = "#df4b26";
+    context.lineJoin = "round";
+    //context.lineWidth = 5;
+
+    for (var i = 0; i < xsArr.length; i++) {
+        context.beginPath();
+        if (dragArr[i] && i) {
+            context.moveTo(xsArr[i - 1], ysArr[i - 1]);
+        } else {
+            context.moveTo(xsArr[i] - 1, ysArr[i]);
+        }
+        context.lineTo(xsArr[i], ysArr[i]);
+        context.closePath();
+        context.strokeStyle = colorsArr[i];
+        context.lineWidth = sizesArr[i];
+        context.stroke();
+    }
+
+}
+
+document.getElementById('red_btn').onclick = function () {
+    currentColor = redColor;
+}
+
+document.getElementById('blue_btn').onclick = function () {
+    currentColor = blueColor;
+}
+
+document.getElementById('green_btn').onclick = function () {
+    currentColor = greenColor;
+}
+
+document.getElementById('yellow_btn').onclick = function () {
+    currentColor = yellowColor;
+}
+
+document.getElementById('purple_btn').onclick = function () {
+    currentColor = purpleColor;
+}
+
+document.getElementById('small_btn').onclick = function() {
+    currentSize = smallSize;
+}
+
+document.getElementById('medium_btn').onclick = function() {
+    currentSize = mediumSize;
+}
+
+document.getElementById('large_btn').onclick = function() {
+    currentSize = largeSize;
+}
+
+document.getElementById('huge_btn').onclick = function() {
+    currentSize = hugeSize;
+}
+
+document.getElementById('clear_btn').onclick = function() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    xsArr = new Array();
+    ysArr = new Array();
+    dragArr = new Array();
+    colorsArr = new Array();
+    sizesArr = new Array();
+}
+
+document.getElementById('marker_btn').onclick = function() {
+    currentTool = "marker";
+}
+
+document.getElementById('eraser_btn').onclick = function() {
+    currentTool = "eraser";
+}
